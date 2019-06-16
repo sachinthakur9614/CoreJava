de47b5b2143e12972d8971cf638774a5a4f76d8f
sachinthakur9614 <sachin.thakur@mca.christuniversity.in>
2019-06-16 19:29:52 +0530

    Adding clear screen method and other stuff

--- LAB 1 PRIMITIVE DATA TYPE & OPERATIONS/Organiser.java
+++ LAB 1 PRIMITIVE DATA TYPE & OPERATIONS/Organiser.java
@@ -3,17 +3,18 @@
 
 
 import java.util.NoSuchElementException;
-import java.io.IOException; 
+import java.io.IOException;
+import java.io.*; 
 import java.util.Scanner; 
 import java.util.*;
- class Hackathon{
+ class Hackathon {
 		private short userId;
 		String firstName;
 		private	String lastName;
 		private	String userName;
-		private int mobileNo;
+		private Long mobileNo;
 		private String email;
-	public void Hackathon(short userId,String firstName,String lastName,int mobileNo,String email)
+	public void Hackathon(short userId,String firstName,String lastName,Long mobileNo,String email)
 	{
 			userId = this.userId;
 			firstName = this.firstName;
@@ -27,46 +28,46 @@ import java.util.*;
 	
 		System.out.println("\n\tREAD USER BASIC INFORMATION!");
 		Scanner sc = new Scanner(System.in); 
-		System.out.println("\nEnter User Id");
-		
+		System.out.println("\nEnter User Id");		
 		userId = sc.nextShort();
 		System.out.print("\nEnter First Name:");
 		firstName = sc.next();		
 		System.out.println("\nEnter Last Name:");
 		lastName = sc.next();
 		System.out.println("\nEnter Mobile Number:");
-		mobileNo = sc.nextInt();
+		mobileNo = sc.nextLong();
 		System.out.println("\nEnter Email:");
-		email = sc.nextLine();
-				sc.close();
+		email = sc.next();
+		sc.close();
 	
 
 	}
-	public void  DisplayUSerInfo()
+	public void  displayUSerInfo()
 	{
 		System.out.println("\n \tDISPLAY USER BASIC INFORMATION");
 		System.out.println("\nUser Id:"+userId);
 		System.out.println("\nFirst Name:"+firstName);
 		System.out.println("\nLast Name:"+lastName);
-		System.out.println("\nMobile No.:"+lastName);
-		System.out.println("\nEmail:"+lastName);
+		System.out.println("\nMobile No.:"+mobileNo);
+		System.out.println("\nEmail:"+email);
 		// System.out.print("Password:"+password);
 	}
 }
 
-public class Organiser extends Hackathon{
-	private String eventName;
-	private  String organisedBy;
-	private String organiserEmail;
-	private String conatctPersonName;
-	private short numberOfEvents;
+public class Organiser extends Hackathon 
+{
+	int eventName;
+	String organisedBy;
+	String organiserEmail;
+	String conatctPersonName;
+	short numberOfEvents;
 	private float eventCharge;
 	private Date startDate;
 	private Date endDate;
 	private int teamMember;
-	private long  contactMobileName;
-		public void Organiser(String eventName,String organisedBy,String organiserEmail,String conatctPersonName,short numberOfEvents,
-							float eventCharge,int teamMember,long contactMobileName) 
+	private Long  contactMobileName;
+	public void Organiser(int eventName, String organisedBy, String organiserEmail, String conatctPersonName, short numberOfEvents,
+							float eventCharge, int teamMember, Long contactMobileName) 
 		{
 			eventName = this.eventName;
 			organisedBy = this.organisedBy;
@@ -77,30 +78,33 @@ public class Organiser extends Hackathon{
 			teamMember = this.teamMember;
 			contactMobileName = this.contactMobileName;
 		}
-public void eventDetails()
+	public void eventDetails()
 		 {
-		 	System.out.println("\n \tREADING ORGANISER BASIC INFORMATION");
-		 	Scanner sc = new Scanner(System.in); 
-		 	System.out.print("\nEnter Event Name:");
-		 	eventName = sc.nextLine();
+		 	
+		 	System.out.print("\n\tREADING ORGANISER BASIC INFORMATION");
+		 	Scanner scan = new Scanner(System.in); 
+		 	cls();
+		 	System.out.println("\nEnter Event Name:");
+		 	eventName = scan.nextInt();
 			System.out.print("\nEnter Organised By:");
-			organisedBy = sc.nextLine();
+			organisedBy = scan.next();
 			System.out.print("\nEnter Organised Email:");
-			organiserEmail = sc.nextLine();		 
+			organiserEmail = scan.next();		 
 			System.out.print("\nEnter Contact Person:");
-			conatctPersonName = sc.nextLine();
+			conatctPersonName = scan.next();
 			System.out.print("\nEnter Number Of Events :");
-			numberOfEvents = sc.nextShort();
+			numberOfEvents = scan.nextShort();
 			System.out.print("\nEnter Event Charge:");
-			eventCharge = sc.nextFloat();
+			eventCharge = scan.nextFloat();
 			System.out.print("\nEnter Number of Team Member:");
-			teamMember = sc.nextInt();
+			teamMember = scan.nextInt();
 			System.out.print("\nEnter Start Date:");
 			startDate = new Date();
 			System.out.print("\nEnter End Date:");
 			endDate = new Date();
 			System.out.print("\nConact Mobile Number:");
-			contactMobileName = sc.nextLong();
+			contactMobileName =scan.nextLong();
+			scan.close();
 
 	 }
  public void displayEventDetails()
@@ -112,32 +116,83 @@ public void eventDetails()
 			System.out.println("\nNumber Of Events:"+numberOfEvents);
 			System.out.println("\nEvent Charge:"+eventCharge);
 			System.out.println("\nNumber of Team Members:"+teamMember);
-			System.out.println("\nEnter Start Date:"+startDate);
-			System.out.println("\nEnter End Date:"+endDate);
+			System.out.println("\n Start Date:"+startDate);
+			System.out.println("\nEnd Date:"+endDate);
+			System.out.println("\nContact Mobile No.:"+endDate);
+				
 		 }
-public static void main(String args[])
+public static void main(String args[]) throws Exception
 		{
 			System.out.println("***Program Name: Write a program to demonstrate various data types and operators.***\n");
 			System.out.println("***********************Author: Sachin Thakur***********************\n");
 			System.out.println("***********************Reg. No: 1847250***********************\n");
-			Hackathon user = new Hackathon();
-			System.out.println("\n\tUSER INFORMATION!\n");
-			try { 
-			user.readUserInfo();
-		
-			}
-
-		catch (Exception e)
-		 {
-	 			user.readUserInfo();
-        
-  		  } 
-    	
 			
-			user.DisplayUSerInfo();
-			Organiser org = new Organiser();
-			System.out.println("ORGANISER INFORMATION!\n");
-			org.eventDetails();
-			org.displayEventDetails();
-		}
+			
+				
+			 
+
+
+			 int choice;
+			int ch;
+			 Scanner scan = new Scanner(System.in); 
+			 System.out.println("Enter Your Choice!");
+			 choice = scan.nextInt();
+try {
+
+
+			 do {
+
+			 switch(choice){
+
+
+			 	case 1:
+			 	System.out.println("\n\tUSER INFORMATION!\n");
+			 	Hackathon usern = new Hackathon();
+			 	usern.readUserInfo();
+				usern.displayUSerInfo();
+
+
+
+			 	break;
+			 	case 2:
+
+				 System.out.println("\n\tORGANISER INFORMATION!");
+			 	Organiser org = new  Organiser();
+			 	org.eventDetails();
+				org.displayEventDetails();
+
+			 	break;
+			 	
+			 	default:
+			 	System.out.print("End Off!");
+			 }
+
+			  System.out.print("You Want To Continue(Y==1/N==2)!");
+			   ch = scan.nextInt();
+
+		
+ 
+			} while(ch==1);
+}
+catch(Exception e)
+System.out.print(e);
+
+			
+
+  		 		 
+			
+}
+
+// This function definition clear the screen of cmd 
+public static void cls()
+{
+	try 
+	{
+		new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+
+	} catch(Exception e)
+	{
+		
+	} 
+}
 }
